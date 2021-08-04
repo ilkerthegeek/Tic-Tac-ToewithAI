38f7cb7b6a4fa4dd2193079ce9518f3a6aa504b8
ilker <ilkerozturketu@gmail.com>
2021-08-05 00:38:52 +0300

    there are some errors but when checking empty spots but I will fix it

--- player.py
+++ player.py
@@ -1,27 +1,31 @@
 import math
 import random
 
+
 class Player:
-    def __init__(self,letter):
+    def __init__(self, letter):
         # letter is X or O
         self.letter = letter
-    def get_move(self,game):
+
+    def get_move(self, game):
         pass
 
+
 class RandomComputerPlayer(Player):
-    def __init__(self,letter):
+    def __init__(self, letter):
         super().__init__(letter)
-    def get_move(self,game):
+
+    def get_move(self, game):
         square = random.choice(game.available_moves())
         return square
 
 
 class HumanPlayer(Player):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, letter):
+        super().__init__(letter)
 
-    def get_move(self,game):
-        valid_square =  False
+    def get_move(self, game):
+        valid_square = False
         val = None
         while not valid_square:
             square = input(self.letter + '\'s turn. Input move (0-9):')
@@ -36,4 +40,3 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid square. Try again!')
         return val
-
